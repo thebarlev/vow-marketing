f5692e5667f0c4070c706067fb7be6a450c4182e
Itzik Barlev <itzikbab@gmail.com>
2026-02-18 14:58:52 +0200

    Fixing h2 title on wide screen

--- src/app/_components/home/SuccessSection.tsx
+++ src/app/_components/home/SuccessSection.tsx
@@ -3,8 +3,8 @@ export function SuccessSection() {
     <section aria-label="הצלחה" className="py-[var(--space-section)] bg-[#F4F1EC]">
       <div className="mx-auto max-w-[1440px] px-4 sm:px-6 lg:px-4 ">
         <div className="space-y-3">
-          <h2 className="text-right">הצלחה לא נבנית במקרה</h2>
-          <h3 className="text-right">
+          <h2 className="text-right sm:text-center">הצלחה לא נבנית במקרה</h2>
+          <h3 className="text-right sm:text-center">
             היא נבנית מהחלטות נכונות, תשתית מדויקת וניהול חכם לאורך זמן.
           </h3>
         </div>
