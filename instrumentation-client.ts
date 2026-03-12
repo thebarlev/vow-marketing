a6023d06e1b691a2bbfffee6f7bae168b6264848
Itzik Barlev <itzikbab@gmail.com>
2026-03-12 17:26:18 +0200

    remove local

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,12 @@
 import posthog from 'posthog-js'
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2026-01-30',
-})
+if (typeof window !== "undefined" && window.location.hostname !== "localhost") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-01-30',
+  })
+
+  posthog.register({
+    internal_user: true,
+  })
+}
